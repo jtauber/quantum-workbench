901055fc2ab2229c3bd21353dc825c669fbee3ef
James Tauber <jtauber@jtauber.com>
2014-07-01 19:40:42 -0400

    initial class list

---
+++ qwb.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+class Bench:
+    pass
+
+
+class Source:
+    pass
+
+
+class Box:
+    pass
+
+
+class ColorBox(Box):
+    pass
+
+
+class HardnessBox(Box):
+    pass
+
+
+class Mirror:
+    pass
+
+
+class Detector:
+    pass
+
+
+if __name__ == "__main__":
+    pass
